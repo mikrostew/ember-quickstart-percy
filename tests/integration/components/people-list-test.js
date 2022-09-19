fe36e7f9549e7ff88d347747e4eb6bbf1c3a1e4b
Michael Stewart <mikrostew@gmail.com>
2022-09-19 14:49:07 -0400

    turn this test up to 11

--- tests/integration/components/people-list-test.js
+++ tests/integration/components/people-list-test.js
@@ -7,20 +7,37 @@ module('Integration | Component | people-list', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`<PeopleList />`);
-
     assert.dom(this.element).hasText('');
 
-    // Template block usage:
     await render(hbs`
-      <PeopleList>
-        template block text
-      </PeopleList>
+      <PeopleList
+        @title="List of People"
+      />
+    `);
+    assert.dom(this.element).hasText('List of People');
+
+    this.set('mockModel', [
+      'John "Stumpy" Pepys',
+      'Eric "Stumpy Joe" Childs',
+      'Peter "James" Bond',
+      'Mick Shrimpton',
+      'Joe "Mama" Besser',
+      'Richard "Ric" Shrimpton',
+      'Sammy "Stumpy" Bateman',
+      'Scott "Skippy" Scuffleton',
+      'Chris "Poppa" Cadeau',
+    ]);
+
+    await render(hbs`
+      <PeopleList
+        @title="List of Drummers"
+        @people={{this.mockModel}}
+      />
     `);
 
-    assert.dom(this.element).hasText('template block text');
+    assert.dom(this.element).hasText(/List of Drummers/);
+    assert.dom(this.element).hasText(/John "Stumpy" Pepys/);
+    assert.dom(this.element).hasText(/Scott "Skippy" Scuffleton/);
   });
 });
